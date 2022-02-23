97f92323cc08d3d41cbeb5850b5f3b42cdbb22d6
jailsonveiga <jmdaveiga2012@gmail.com>
2022-02-22 19:05:39 -0500

    Word Guesses - Initial Commit - Java

---
+++ src/WordGuesses.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WordGuesses {
+    public static void main(String[] args) {
+        String[] playableWords = new String[]{
+                "guessing", "these", "words", "is", "very", "easy"
+        };
+    }
+}
